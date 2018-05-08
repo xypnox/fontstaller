b947983479c0038fd5cd734b09e9ac0abd876a27
xypnox <xypnox@gmail.com>
2018-05-08 18:34:30 +0530

    Added argument parsing

--- fontstaller.py
+++ fontstaller.py
@@ -1,14 +1,14 @@
 from zipfile import ZipFile
+import argparse
 
 
 class Fontstaller:
-    def __init__(self, dir='.', exdir='~/.fonts'):
+    def __init__(self, dir):
         self.dir = dir
-        self.exdir = exdir
 
-    def install(self):
+    def install(self, exdir):
         with ZipFile(self.dir, 'r') as myzip:
-            myzip.extract(self.exdir)
+            myzip.extract(exdir)
 
     def checkFont(self):
         with ZipFile(self.dir, 'r') as myzip:
@@ -18,3 +18,17 @@ class Fontstaller:
                 elif font.endswith('otf'):
                     return True
         return False
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Installs fonts in a given folder')
+    parser.add_argument('--fdir',
+                        default='fonts',
+                        help='The directory containing fonts')
+    parser.add_argument('--idir',
+                        default='~./fonts',
+                        help='The directory in which fonts will be installed')
+    parser.add_argument('--ignorezip',
+                        action='store_true',
+                        help='Ignore zipped files')
+    args = parser.parse_args()
